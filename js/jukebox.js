4328cdfb0c010eeafdd76090d951d450d73df260
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-08-25 10:42:15 -0400

    done

--- js/jukebox.js
+++ js/jukebox.js
@@ -15,30 +15,38 @@ function randomNumber() {
 }
 
 function menu() {
-  $('#menu').on('click', function(e) {
+  $('#menu').on('click', menuOptions);
+}
+
+function menuOptions(e) {
     $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
 }
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', playSong);
+}
+
+function playSong(e) {
+  $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  return "now playing " + songToPlay + " by " + theArtist;
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
+  $('#pause').on('click', pauseSong);
+}
+
+function pauseSong(e) {
     $('#songPaused').html(songToPlay + " is paused");
-  });
 }
 
 function show() {
-  $('#show').on('click', function(e) {
+  $('#show').on('click', availableSongs);
+}
+
+function availableSongs(e) {
     var songs = "";
     for (var key in jukeboxSongs) {
       songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
     }
     $('#showSongs').html(songs);
-  });
 }
