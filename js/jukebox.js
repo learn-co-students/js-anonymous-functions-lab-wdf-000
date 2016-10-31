8beab84d35ba87b243a792099592a33703fc4e0d
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-31 11:36:10 -0400

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
 var artists = Object.keys(jukeboxSongs);
-var theArtist = artists[Math.floor(Math.random()) * artists.length];
+var theArtist = artists[Math.floor(Math.random() * artists.length)];
 var songToPlay = jukeboxSongs[theArtist];
 
 function randomNumber() {
@@ -15,30 +15,41 @@ function randomNumber() {
 }
 
 function menu() {
-  $('#menu').on('click', function(e) {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  $('#menu').on('click', handleMenuClick);
 }
+
+function handleMenuClick(e) {
+    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+}
+
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', handlePlayClick); 
 }
 
+function handlePlayClick(e) {
+    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+    return "now playing " + songToPlay + " by " + theArtist;
+}
+
+
 function pause() {
-  $('#pause').on('click', function(e) {
+  $('#pause').on('click', handlePauseClick); 
+}
+
+function handlePauseClick(e) {
     $('#songPaused').html(songToPlay + " is paused");
-  });
 }
 
 function show() {
-  $('#show').on('click', function(e) {
+  $('#show').on('click', handleShowClick);
+}
+
+function handleShowClick(e) {
     var songs = "";
     for (var key in jukeboxSongs) {
       songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
     }
     $('#showSongs').html(songs);
-  });
 }
+
